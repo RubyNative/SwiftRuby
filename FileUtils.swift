afe264a666c3dbc3e5c681724e263aebbdb68c55
John Holdsworth <github@johnholdsworth.com>
2015-10-31 21:16:50 +0000

    No spawn for iOS

--- FileUtils.swift
+++ FileUtils.swift
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 30/09/2015.
 //  Copyright © 2015 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/RubyNative/File.swift#3 $
+//  $Id: //depot/SwiftRuby/FileUtils.swift#13 $
 //
 //  Repo: https://github.com/RubyNative/SwiftRuby
 //
