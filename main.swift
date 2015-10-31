6fc8aef8b7bf471b31e71cf2d5faabe340e4f863
John Holdsworth <github@johnholdsworth.com>
2015-10-31 22:16:53 +0000

    Restore main.swift

---
+++ main.swift
@@ -0,0 +1,16 @@
+#!/usr/bin/env diamond
+//
+//  main.swift
+//  SwiftRuby
+//
+//  Created by John Holdsworth on 18/09/2015.
+//  Copyright © 2015 John Holdsworth. All rights reserved.
+//
+//  $Id: //depot/SwiftRuby/main.swift#5 $
+//
+//  Repo: https://github.com/RubyNative/SwiftRuby
+//
+
+import Foundation
+
+print("Hello, \(Process.arguments)!")
